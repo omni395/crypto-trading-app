a72970922b08f44e125a7a75c12c4c6abe8d9ab8
omni395 <omni395@gmail.com>
2025-04-08 15:36:04 +0300

    yaml

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { createHtmlPlugin } from 'vite-plugin-html'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    createHtmlPlugin({})],
   server: {
     host: "127.0.0.1",
     port: 8080
